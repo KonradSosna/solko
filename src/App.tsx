8131e2dc3db2820c18bd57401997d260dc42bed5
Konrad Sosna <konrad.sosna@lastminute.com>
2025-01-25 20:07:22 +0100

    delete router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './Components/Home';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />}>
-					<Route index element={<Home />} />
-					<Route path="blogs" element={<Home />} />
-					<Route path="contact" element={<Home />} />
-					<Route path="*" element={<Home />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<Home />
 	);
 }
 
